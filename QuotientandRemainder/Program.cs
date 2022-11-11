3266ee7431a5b487b81962ad77b45499d0cada11
challaramakrishnareddy52 <challaramakrishnareddy52@gmail.com>
2022-11-11 18:40:34 +0530

    Quotient and Remainder

---
+++ QuotientandRemainder/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace QuotientandRemainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dividend = 50, divisor = 8;
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+            Console.WriteLine("Dividend : {0} Divisor : {1}",dividend,divisor);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+        }
+    }
+}
